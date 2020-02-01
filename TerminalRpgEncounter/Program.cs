5dc3321c9cd5dddb8ad8440adfc6fb001cde8546
Radeax <joflip@gmail.com>
2020-02-01 17:46:51 -0500

    Created Food/Buffet/Ninja classes

--- TerminalRpgEncounter/Program.cs
+++ TerminalRpgEncounter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TerminalRpgEncounter
 {
@@ -6,13 +7,16 @@ namespace TerminalRpgEncounter
   {
     static void Main(string[] args)
     {
-      Human ninja1 = new Human("ninja1");
-      Human ninja2 = new Human("ninja2");
+      Ninja ninja = new Ninja();
+      Buffet buffet = new Buffet();
 
-      Console.WriteLine($"ninja2 has {ninja2.Health} HP");
-      ninja1.Attack(ninja2);
-      Console.WriteLine("ninja1 attacks ninja2!");
-      Console.WriteLine($"ninja2 now has {ninja2.Health} HP");
+      ninja.Eat(buffet.Serve());
+
+      while (!ninja.isFull)
+      {
+        ninja.Eat(buffet.Serve());
+      }
+      ninja.Eat(buffet.Serve());
     }
   }
 
@@ -51,6 +55,77 @@ namespace TerminalRpgEncounter
     {
       target.health -= Strength * 5;
       return target.health;
+    }
+  }
+
+  class Food
+  {
+    public string Name;
+    public int Calories;
+    public bool isSpicy;
+    public bool isSweet;
+
+    public Food(string name, int cal, bool spicy, bool sweet)
+    {
+      Name = name;
+      Calories = cal;
+      isSpicy = spicy;
+      isSweet = sweet;
+    }
+  }
+
+  class Buffet
+  {
+    public List<Food> Menu;
+
+    public Buffet()
+    {
+      Menu = new List<Food>()
+      {
+          new Food("Pizza", 200, false, false),
+          new Food("Fried Rice", 228, false, false),
+          new Food("Mango", 201, false, true),
+          new Food("Buffalo Wings", 143, true, true),
+          new Food("Jalapeno", 26, true, false),
+          new Food("Ice Cream", 137, false, true),
+          new Food("Ribs", 299, false, false),
+      };
+    }
+
+    public Food Serve()
+    {
+      Random rand = new Random();
+      return Menu[rand.Next(0, Menu.Count)];
+    }
+  }
+
+  class Ninja
+  {
+    private int calorieIntake;
+    public List<Food> FoodHistory;
+
+    public Ninja()
+    {
+      FoodHistory = new List<Food>();
+    }
+
+    public bool isFull
+    {
+      get { return calorieIntake > 1200; }
+    }
+
+    public void Eat(Food item)
+    {
+      if (!isFull)
+      {
+        calorieIntake += item.Calories;
+        FoodHistory.Add(item);
+        Console.WriteLine($"Ninja ate {item.Name} | Spicy: {item.isSpicy} | Sweet: {item.isSweet}");
+      }
+      else
+      {
+        Console.WriteLine("Ninja is full and cannot eat anymore food.");
+      }
     }
   }
 }
