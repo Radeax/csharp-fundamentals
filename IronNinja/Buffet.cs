705aaf23692ab384f427856c41434ea8e5873d92
Radeax <joflip@gmail.com>
2020-02-02 19:13:43 -0500

    Created IronNinja

---
+++ IronNinja/Buffet.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+namespace IronNinja
+{
+  class Buffet
+  {
+    public List<IConsumable> Menu;
+
+    public Buffet()
+    {
+      Menu = new List<IConsumable>()
+      {
+          new Food("Pizza", 200, false, false),
+          new Food("Fried Rice", 228, false, false),
+          new Food("Mango", 201, false, true),
+          new Food("Buffalo Wings", 143, true, true),
+          new Food("Jalapeno", 26, true, false),
+          new Food("Ice Cream", 137, false, true),
+          new Food("Ribs", 299, false, false),
+          new Drink("Water", 0, false, false),
+          new Drink("Coke", 140, false, true),
+          new Drink("Tea", 2, false, false),
+          new Drink("Moscow Mule", 182, true, false)
+      };
+    }
+
+    public IConsumable Serve()
+    {
+      Random rand = new Random();
+      return Menu[rand.Next(0, Menu.Count)];
+    }
+  }
+}
---
+++ IronNinja/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IronNinja
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Buffet buffet = new Buffet();
+      SweetTooth sweettooth = new SweetTooth();
+      SpiceHound spicehound = new SpiceHound();
+
+      while (!sweettooth.IsFull)
+      {
+        sweettooth.Consume(buffet.Serve());
+      }
+      int spiceCount = sweettooth.ConsumptionHistory.Count;
+
+      while (!spicehound.IsFull)
+      {
+        spicehound.Consume(buffet.Serve());
+      }
+      spicehound.Consume(buffet.Serve());
+
+
+      int sweetCount = spicehound.ConsumptionHistory.Count;
+
+      if (spiceCount > sweetCount)
+      {
+        Console.WriteLine($"SweetTooth consumed the most, consuming {sweetCount} items!");
+      }
+      else
+      {
+        Console.WriteLine($"SpiceHound consumed the most, consuming {spiceCount} items!");
+      }
+    }
+  }
+}
