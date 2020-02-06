8a071c4b13374a9c15740144e72097458f768913
Radeax <joflip@gmail.com>
2020-02-05 21:44:08 -0500

    Testing methods

--- DeckOfCards/Program.cs
+++ DeckOfCards/Program.cs
@@ -2,11 +2,41 @@
 
 namespace DeckOfCards
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      // Create Player
+      Player myPlayer = new Player("Jo");
+
+      // Create Deck
+      Deck myDeck = new Deck();
+      myDeck.Shuffle();
+
+      // Draw 5 cards
+      myPlayer.Draw(myDeck);
+      myPlayer.Draw(myDeck);
+      myPlayer.Draw(myDeck);
+      myPlayer.Draw(myDeck);
+      myPlayer.Draw(myDeck);
+
+      // Display cards
+      foreach (Card card in myPlayer.hand)
+      {
+        Console.WriteLine($"{card._stringVal} of {card._suit}");
+      }
+
+      Console.WriteLine("Choose card to discard: ");
+      string discard = Console.ReadLine();
+      int discardNum = Int32.Parse(discard);
+      myPlayer.Discard(discardNum);
+
+      // Display cards
+      foreach (Card card in myPlayer.hand)
+      {
+        Console.WriteLine($"{card._stringVal} of {card._suit}");
+      }
+
     }
+  }
 }
