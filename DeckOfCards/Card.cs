88d480ce7c7d7c62e668d05066a604ab1f1f1ae2
Radeax <joflip@gmail.com>
2020-02-05 20:14:07 -0500

    Card.cs: created constructor method

--- DeckOfCards/Card.cs
+++ DeckOfCards/Card.cs
@@ -4,8 +4,15 @@ namespace DeckOfCards
 {
   class Card
   {
-    string stringVal;
-    string suit;
-    int val;
+    string _stringVal;
+    string _suit;
+    int _val;
+
+    public Card(string stringVal, string suit, int val)
+    {
+      _stringVal = stringVal;
+      _suit = suit;
+      _val = val;
+    }
   }
 }
